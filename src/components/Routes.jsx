cea3b2890f0a1e13bb50c484bc73432550f03ba3
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-02-18 16:09:39 +0545

    add: routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes as Switch, Route, Navigate } from 'react-router-dom'
+import Results from './Results'
+
+const renderMultiRoutes = ({ element: Element, paths, ...rest }: MultiRoutes) =>
+  paths.map((path) => <Route key={path} path={path} {...rest} element={Element} />);
 
 const Routes = () => {
   return (
-    <div>
-        <h1>Routes</h1>
+    <div className="p-4">
+        <Switch>
+            <Route exact path="/" element={<Navigate replace to="/search" />} />
+            {renderMultiRoutes({
+                paths: ['/search', '/images', '/news', '/videos'],
+                element: <Results />,
+                exact: true
+            })}
+        </Switch>
     </div>
   )
 }
